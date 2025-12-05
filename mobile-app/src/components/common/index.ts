1dc0807877d07654689b1ea6e1606d9567ebcced
KarimF430 <161332053+KarimF430@users.noreply.github.com>
2025-12-05 17:39:36 +0530

    Ignore mobile-app until development complete

--- mobile-app/src/components/common/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * MotorOctane Mobile App - Common Components Index
- */
-
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-export { default as Badge } from './Badge';
--- mobile-app/src/theme/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * MotorOctane Mobile App - Theme Index
- * Export all theme utilities
- */
-
-export * from './colors';
-export * from './typography';
-export * from './spacing';
