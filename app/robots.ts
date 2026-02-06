e520078e61f97b46b80dba2dc4e5dc024825a583
Karim <karim@Karims-MacBook-Air.local>
2026-02-07 01:36:23 +0530

    Update robots.txt to allow AI bots

--- app/robots.ts
+++ app/robots.ts
@@ -4,21 +4,38 @@ export default function robots(): MetadataRoute.Robots {
     const baseUrl = 'https://www.gadizone.com'
 
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: [
-                '/api/',
-                '/admin/',
-                '/login',
-                '/signup',
-                '/forgot-password',
-                '/reset-password',
-                '/verify-email',
-                '/debug-env',
-                '/test-honda',
-            ],
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: [
+                    '/api/',
+                    '/admin/',
+                    '/login',
+                    '/signup',
+                    '/forgot-password',
+                    '/reset-password',
+                    '/verify-email',
+                    '/debug-env',
+                    '/test-honda',
+                ],
+            },
+            {
+                userAgent: 'GPTBot',
+                allow: '/',
+                disallow: [],
+            },
+            {
+                userAgent: 'Google-Extended',
+                allow: '/',
+                disallow: [],
+            },
+            {
+                userAgent: 'CCBot',
+                allow: '/',
+                disallow: [],
+            }
+        ],
         sitemap: `${baseUrl}/sitemap.xml`,
     }
 }
